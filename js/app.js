675bb5cc7e3f84abfa72c09df413b4fa9462cff5
Mahmoud alzoubi <mahmoudalzoubi073@gmail.com>
2021-02-13 14:00:29 +0200

    fixed a logical error

--- js/app.js
+++ js/app.js
@@ -1,283 +1,72 @@
 "use strict";
 let count = 0;
-let userName2;
 
-let nownMe1 = confirm("gues who am i ? ");
-console.log(nownMe1);
+let userName = prompt("whate is your name ? ");
+userName = userName.toLowerCase();
+alert ('welcome' + userName)
 
-if (nownMe1) {
-  alert("welcome my friend ");
-} 
-else { 
-   alert("I hope to know you soon but now lets continue  ");
- }
-
-
-
-
-
-
-
-
-
-
-// if (myAge == "y") {
-
- 
-//   let userGues = prompt("please enter what you are guessing");
-
-//   if (userGues == 26) {
-//     alert("yes , you are true , my age is 26  ");
-//   }
- 
-// else if (myAge == "n") {
-//   alert("ok , my age is 26 ");
-
-// }
-// }
-
-
-
-
-
-
-
-
-
-
-let sports = prompt("what is my favorit sport ?");
-sports = sports.toLowerCase();
-
-if (sports == "football") {
-  count++
-  alert(" yes , i love football and i am agood player ");
-} 
-else {
-
-       alert("no , i love football and i am agood player ");
-
-  }
-
-
-
-
-//first qsn
-let myAge = prompt("can you gues my age ? (y/n) ");
-console.log(myAge);
+let myAge = prompt("do you think i'm 25 years old ? (y/n) ");
 myAge = myAge.toLowerCase();
-let age = prompt("what is my age ?");
-parseInt(age);
-if (age == 26) {
+if (myAge == "yes" || myAge == "y" ) {
   count++
-  alert(" yes ,  my age is 26 ");
-} else {
+  alert("correct , my age is 25 ");
+} 
+else if(myAge == "no" || myAge == "n") {
+  alert("no , my age is 25 ");
+}
 
-  for(let i=0 ; i<4 ; i++){
+let myHight = prompt("do you think my hight 185 ? (y/n) ");
+myHight = myHight.toLowerCase();
+if (myHight == "yes" || myHight == "y" ) {
+  alert("no , my hight is 188cm ");
+} 
+else if(myHight == "no" || myHight == "n") {
+  count++
+  alert("correct , my hight is 188cm ");
+}
 
-    // let chance4 = prompt('no ,try again')
-
-    if (age<26){
-      alert ('no it your guess less than my age ')
-      age = prompt("what is my age ?");
-
-      }
-      else if (age>26){
-        alert ('no it your guess bigger than my age ')
-        age = prompt("what is my age ?");
-
-      }
- 
-    if (age==26){
-      //alert(" yes , my hight is 188cm ");
-
-break
-
-    }
-     
-  }
-  alert("ok , my age is 26 ");
+let myWight = prompt("do you think my wight 80 ? (y/n) ");
+myWight = myWight.toLowerCase();
+if (myWight == "yes" || myWight == "y" ) {
+  count++
+  alert("correct , my wight is 80 ");
+} 
+else if(myWight == "no" || myWight == "n") {
+  alert("no ,  my wight is 80 ");
 }
 
 
 
-
-
-
-
-
-
-
-
-//second qsn
-let hight = prompt("what is my hight ?");
-parseInt(hight);
-if (hight == 188) {
+let favSport = prompt("do you think my favorit sport is football ? (y/n) ");
+favSport = favSport.toLowerCase();
+if (favSport == "yes" || favSport == "y" ) {
   count++
-  alert(" yes , my hight is 188cm ");
-} else {
-
-  for(let i=0 ; i<3 ; i++){
-
-    // let chance4 = prompt('no ,try again')
-
-    if (hight<188){
-      alert ('no it your guess less than my hight ')
-       hight = prompt("what is my hight ?");
-
-      }
-      else if (hight>188){
-        alert ('no it your guess bigger than my hight ')
-        hight = prompt("what is my hight ?");
-
-      }
- 
-    if (hight==188){
-      //alert(" yes , my hight is 188cm ");
-
-break
-
-    }
-     
-  }
-
-  alert("ok , my hight is 188cm ");
+  alert("sure , my favorit sport is football ");
+} 
+else if(favSport == "no" || favSport == "n") {
+  alert("no , my favorit sport is football ");
 }
 
 
-
-
-
-
-//third qsn
-let wight = prompt("what is my wight ?");
-parseInt(wight);
-if (wight == 80) {
+let movies = prompt("do you think i like movies ? (y/n) ");
+movies = movies.toLowerCase();
+if (movies == "yes" || movies == "y" ) {
   count++
-  alert(" yes , my hight is 188cm ");
-} else {
-
-  for(let i=0 ; i<4 ; i++){
-
-    // let chance4 = prompt('no ,try again')
-
-    if (wight<80){
-      alert ('no it your guess less than my wight ')
-      wight = prompt("what is my wight ?");
-
-      }
-      else if (wight>80){
-        alert ('no it your guess bigger than my wight ')
-        wight = prompt("what is my wight ?");
-
-      }
- 
-    if (wight==80){
-      //alert(" yes , my hight is 188cm ");
-
-break
-
-    }
-     
-  }
-
-  alert("ok , my wight is 80 ");
+  alert("correct , i like movies ");
+} 
+else if(movies == "no" || movies == "n") {
+  alert("no , i like movies ");
 }
 
-
-
-
-
-
-// //4th qsn
-// let batchNum = prompt("how many people in our batch ?");
-// parseInt(batchNum);
-// if (batchNum == 30) {
-//   alert(" yes , there is 30 people in our batch ");
-// } else {
-
-//   for(let i=0 ; i<4 ; i++){
-
-//     // let chance4 = prompt('no ,try again')
-
-//     if (batchNum<30){
-//       alert ('no , your guess is less than the number ')
-//       batchNum = prompt("how many people in our batch ?");
-
-//       }
-//       else if (batchNum>30){
-//         alert ('no , your guess is bigger than the number ')
-//         batchNum = prompt("how many people in our batch ?");
-
-//       }
- 
-//     if (batchNum==30){
-//       //alert(" yes , my hight is 188cm ");
-
-// break
-
-//     }
      
-//   }
 
-//   alert("ok , there is 30 people in our batch ");
-// }
-
-
-
-
-
-// //5th qsn 
-// let favNum = prompt("Can you guess my favorit number? from (1 - 20)");
-// parseInt(favNum);
-// if (favNum == 7) {
-//   alert(" yes , there is 30 people in our batch ");
-// } else {
-
-//   for(let i=0 ; i<4 ; i++){
-
-//     // let chance4 = prompt('no ,try again')
-
-//     if (favNum<7){
-//       alert ('no , your guess is less than the number ')
-//       favNum = prompt("Can you guess my favorit number? from (1 - 20)");
-
-//       }
-//       else if (favNum>7){
-//         alert ('no , your guess is bigger than the number ')
-//         favNum = prompt("Can you guess my favorit number? from (1 - 20)");
-
-//       }
- 
-//     if (favNum==7){
-//       //alert(" yes , my hight is 188cm ");
-
-// break
-
-//     }
-     
-//   }
-
-//   alert("ok , my favorit number is 7 ");
-// }
-
-
-
-
-
-
-
-//6th qsn 
 let zlaNum = prompt("Can you guess the zlatan ibrahimovich number in ACmila club ? ");
 parseInt(zlaNum);
 if (zlaNum == 10) {
   alert(" yes , the zlatan ibrahimovich number is 10  ");
   count++
 } else {
-
-  for(let i=0 ; i<4 ; i++){
-
-    // let chance4 = prompt('no ,try again')
-
+ for(let i=0 ; i<4 ; i++){
     if (zlaNum<10){
       alert ('no , your guess is less than the number ')
       zlaNum = prompt("Can you guess the zlatan ibrahimovich number in ACmila club ? ");
@@ -290,63 +79,41 @@ if (zlaNum == 10) {
       }
  
     if (zlaNum==10){
-      //alert(" yes , my hight is 188cm ");
-
-break
-
+        break
     }
-     
   }
-
   alert("ok , the zlatan ibrahimovich number is 10 ");
 }
 
-
-
-
-
-
+let x = false ;
 let player = ['cr7', 'messi', 'zlatan','zidan'];
 for(let i = 0 ; i < 6 ; i++){
     let userInp = prompt('can you guess my favourite player ?')
     let answer = userInp.toLocaleLowerCase();
-    for(let j = 0; j<mySeson.length;j++)
+    for(let j = 0; j<player.length;j++)
     {
-        if(answer=== mySeson[j]){
+        if(answer=== player[j]){
             alert('correct , i hope you are prefer the same player ');
-            count++;
-            i = 6 ;
+           count++; 
+            x=true ;
             break;
         }
+   
     }
-    if (i !== 6){
-
-      alert('wrong , my favourite player is cr7 ');
-
-    }
-alert('you are git '+ count + 'out of 7' )
+    if (x === true){
+      break;
   }
 
+  }
+if (x === false){
+  alert('wrong , my favorite players '+ player );
+}
+  alert('you are git '+ count + 'out of 7' );
+
+ 
 
 
 
-
-
-
-
-
-
-
-
-
-// let passion = prompt("does the programming is your passion ?(yes/no) ");
-// if (passion == "yes") {
-//   alert("i agree with you , i hope you to reach to your goal  ");
-// } else {
-//   alert(
-//     " i do not agree with you , i think the programming is important and you should keep in your mind to learn the programming "
-//   );
-// }
 
 
 
